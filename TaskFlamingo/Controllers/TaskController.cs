2ee8e7077dd3efb7b8702b9ae43bccc6bc91be40
Kyle Baley <kyle@clear-measure.com>
2015-04-25 14:38:31 -0400

    Refactored command handler to domain service

--- TaskFlamingo/Controllers/TaskController.cs
+++ TaskFlamingo/Controllers/TaskController.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Web.Http;
 using TaskFlamingo.Domain;
 
 namespace TaskFlamingo.Controllers
 {
-  using System.Data.SqlClient;
-
-  using Dapper;
-
   public class TaskController : ApiController
   {
     public ScheduleTaskDto Get(string id)
@@ -19,9 +14,8 @@ namespace TaskFlamingo.Controllers
 
     public void Post([FromBody]ScheduleTaskDto dto)
     {
-      var command = new ScheduleTaskCommand(dto);
-      var commandHandler = new CommandHandler();
-      commandHandler.Handle(command);
+      var taskService = new TaskService();
+      taskService.ScheduleTask(dto);
     }
 
     // PUT api/values/5
@@ -35,81 +29,6 @@ namespace TaskFlamingo.Controllers
     }
   }
 
-  public class CommandHandler
-  {
-    public void Handle(ScheduleTaskCommand command)
-    {
-      var task = CreateTaskFrom(command.ScheduleTaskDto);
-      //save task command to db
-      using (var connection = GetOpenConnection())
-      {
-        connection.Execute(@"
-            INSERT INTO [dbo].[Tasks]
-                       ([TaskId]
-                       ,[Name]
-                       ,[DueDate]
-                       ,[Instructions]
-                       ,[Status]
-                       ,[CompletionDate]
-                       ,[CompletionComment])
-                 VALUES
-                       (@taskId
-                       ,@name
-                       ,@dueDate
-                       ,@instructions
-                       ,@status
-                       ,@completionDate
-                       ,@completionComment)", 
-                                 new { taskId = task.TaskId, 
-                                   name = task.Name, 
-                                   dueDate = task.DueDate, 
-                                   instructions = task.Instructions,
-                                   status = task.Status,
-                                   completionDate = task.CompletionDate,
-                                   completionComment = task.CompletionComment});
-      }
-
-    }
-
-    private Task CreateTaskFrom(ScheduleTaskDto dto)
-    {
-      var task = new Task
-      {
-        TaskId = Guid.NewGuid(),
-        Name = dto.Name,
-        DueDate = dto.DueDate,
-        Status = TaskStatus.Created,
-        Instructions = dto.Instructions,
-        CompletionDate = null
-      };
-      return task;
-    }
-
-    public static SqlConnection GetOpenConnection()
-    {
-      var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-      return new SqlConnection(connectionString);
-    }
-  }
-
-  public class ScheduleTaskCommand
-  {
-    private readonly ScheduleTaskDto _dto;
-
-    public ScheduleTaskCommand(ScheduleTaskDto dto)
-    {
-      this._dto = dto;
-    }
-
-    public ScheduleTaskDto ScheduleTaskDto
-    {
-      get
-      {
-        return this._dto;
-      }
-    }
-  }
-
   public class ScheduleTaskDto
   {
     public string Name { get; set; }
@@ -117,15 +36,5 @@ namespace TaskFlamingo.Controllers
     public DateTime DueDate { get; set; }
     public List<string> Assignees { get; set; }
   }
-
-  public class ScheduleTask
-  {
-    public string Name { get; set; }
-    public string Instructions { get; set; }
-    public DateTime DueDate { get; set; }
-    public DateTime CompletionDate { get; set; }
-    public string CompletionComment { get; set; }
-  }
-
 
 }
---
+++ TaskFlamingo/Domain/TaskService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+using TaskFlamingo.Controllers;
+
+namespace TaskFlamingo.Domain
+{
+  public class TaskService
+  {
+    public void ScheduleTask(ScheduleTaskDto dto)
+    {
+      var task = CreateTaskFrom(dto);
+      SaveTask(task);
+    }
+
+    private void SaveTask(Task task)
+    {
+      //save task command to db
+      using (var connection = GetOpenConnection())
+      {
+        connection.Execute(@"
+            INSERT INTO [dbo].[Tasks]
+                       ([TaskId]
+                       ,[Name]
+                       ,[DueDate]
+                       ,[Instructions]
+                       ,[Status]
+                       ,[CompletionDate]
+                       ,[CompletionComment])
+                 VALUES
+                       (@taskId
+                       ,@name
+                       ,@dueDate
+                       ,@instructions
+                       ,@status
+                       ,@completionDate
+                       ,@completionComment)",
+          new
+          {
+            taskId = task.TaskId,
+            name = task.Name,
+            dueDate = task.DueDate,
+            instructions = task.Instructions,
+            status = task.Status,
+            completionDate = task.CompletionDate,
+            completionComment = task.CompletionComment
+          });
+      }
+    }
+
+    private Task CreateTaskFrom(ScheduleTaskDto dto)
+    {
+      var task = new Task
+      {
+        TaskId = Guid.NewGuid(),
+        Name = dto.Name,
+        DueDate = dto.DueDate,
+        Status = TaskStatus.Created,
+        Instructions = dto.Instructions,
+        CompletionDate = null
+      };
+      return task;
+    }
+
+    public static SqlConnection GetOpenConnection()
+    {
+      var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+      return new SqlConnection(connectionString);
+    }
+  }
+}
