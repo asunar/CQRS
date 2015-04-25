b1a9dbe21a1346ce15d4d6406485f607ae7909e5
Alper Sunar <ASunar@paylocity.com>
2015-04-25 11:57:26 -0500

    Added Dapper, saving ScheduleTaskDto

--- TaskFlamingo/Controllers/TaskController.cs
+++ TaskFlamingo/Controllers/TaskController.cs
@@ -4,52 +4,97 @@ using System.Web.Http;
 
 namespace TaskFlamingo.Controllers
 {
-    public class TaskController : ApiController
+  using System.Data.SqlClient;
+
+  using Dapper;
+
+  public class TaskController : ApiController
+  {
+    public ScheduleTaskDto Get(string id)
     {
-        public ScheduleTaskDto Get(string id)
-        {
-            return new ScheduleTaskDto {Name = "moo"};
-        }
-
-        public void Post( [FromBody]ScheduleTaskDto dto )
-        {
-            var command = new ScheduleTaskCommand(dto);
-            var commandHandler = new CommandHandler();
-            commandHandler.Handle(command);
-        }
-
-        // PUT api/values/5
-        public void Put( int id, [FromBody]string value ) {
-        }
-
-        // DELETE api/values/5
-        public void Delete( int id ) {
-        }
+      return new ScheduleTaskDto { Name = "moo" };
     }
 
-    public class CommandHandler
+    public void Post([FromBody]ScheduleTaskDto dto)
     {
-        public void Handle(ScheduleTaskCommand command)
-        {
-
-        }
+      var command = new ScheduleTaskCommand(dto);
+      var commandHandler = new CommandHandler();
+      commandHandler.Handle(command);
     }
 
-    public class ScheduleTaskCommand
+    // PUT api/values/5
+    public void Put(int id, [FromBody]string value)
     {
-        private readonly ScheduleTaskDto _dto;
-
-        public ScheduleTaskCommand(ScheduleTaskDto dto)
-        {
-            _dto = dto;
-        }
     }
 
-    public class ScheduleTaskDto
+    // DELETE api/values/5
+    public void Delete(int id)
     {
-        public string Name { get; set; }
-        public string Instructions { get; set; }
-        public DateTime DueDate { get; set; }
-        public List<string> Assignees { get; set; }
     }
+  }
+
+  public class CommandHandler
+  {
+    public void Handle(ScheduleTaskCommand command)
+    {
+      //save task command to db
+      using (var connection = GetOpenConnection())
+      {
+        connection.Execute(@"
+            INSERT INTO [dbo].[Tasks]
+                       ([TaskId]
+                       ,[Name]
+                       ,[DueDate]
+                       ,[Instructions])
+                 VALUES
+                       (@taskId
+                       ,@name
+                       ,@dueDate
+                       ,@instructions)", new { taskId = Guid.NewGuid(), name = command.ScheduleTaskDto.Name, dueDate = command.ScheduleTaskDto.DueDate, instructions = command.ScheduleTaskDto.Instructions });
+      }
+
+    }
+
+    public static SqlConnection GetOpenConnection()
+    {
+      return new SqlConnection(@"Server=localhost\sqlserver2014;Database=TaskFlamingo;Integrated Security=True;");
+    }
+  }
+
+  public class ScheduleTaskCommand
+  {
+    private readonly ScheduleTaskDto _dto;
+
+    public ScheduleTaskCommand(ScheduleTaskDto dto)
+    {
+      this._dto = dto;
+    }
+
+    public ScheduleTaskDto ScheduleTaskDto
+    {
+      get
+      {
+        return this._dto;
+      }
+    }
+  }
+
+  public class ScheduleTaskDto
+  {
+    public string Name { get; set; }
+    public string Instructions { get; set; }
+    public DateTime DueDate { get; set; }
+    public List<string> Assignees { get; set; }
+  }
+
+  public class ScheduleTask
+  {
+    public string Name { get; set; }
+    public string Instructions { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime CompletionDate { get; set; }
+    public string CompletionComment { get; set; }
+  }
+
+
 }
